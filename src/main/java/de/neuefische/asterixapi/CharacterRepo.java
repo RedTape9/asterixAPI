58411b9b95be1a309173e87abc6417c7fa5d97c0
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-24 12:41:38 +0100

    add DTO

--- src/main/java/de/neuefische/asterixapi/CharacterRepo.java
+++ src/main/java/de/neuefische/asterixapi/CharacterRepo.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
 @Repository
 public interface CharacterRepo extends MongoRepository<Character, String>{
     Optional<Character> findByName(String name);
